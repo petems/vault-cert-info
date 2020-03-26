37ede60e51e63d15fdd85b246a96ae39161a60a7
petems <p.morsou@gmail.com>
2020-03-26 12:46:38 +0000

    Add client-inspect for debugging

--- main.go
+++ main.go
@@ -13,11 +13,15 @@ import (
 
 	"github.com/hashicorp/vault/api"
 	"github.com/hashicorp/vault/sdk/helper/certutil"
+	"github.com/sirupsen/logrus"
+	"github.com/whuang8/redactrus"
+	prefixed "github.com/x-cray/logrus-prefixed-formatter"
 
 	// helpers
 	"github.com/cloudflare/cfssl/certinfo"
 	"github.com/hokaccha/go-prettyjson"
 	"github.com/olekukonko/tablewriter"
+	clientInspect "github.com/petems/client-inspect/http"
 	vltcrthlpr "github.com/petems/vault-cert-helpers"
 
 	"github.com/urfave/cli/v2"
@@ -78,6 +82,11 @@ func main() {
 				Value: true,
 				Usage: "Do not output anything other than errors or returned data",
 			},
+			&cli.BoolFlag{
+				Name:  "debug",
+				Value: false,
+				Usage: "Show debug information, with full http logs",
+			},
 		},
 	}
 
@@ -125,12 +134,10 @@ func getENV(value string) (string, error) {
 }
 
 // NewVaultClient creates a new Vault API client for Vault
-func NewVaultClient(vaultAddr, vaultToken string) (*api.Client, error) {
-	var httpClient = &http.Client{
-		Timeout: 10 * time.Second,
-	}
+func NewVaultClient(httpClient *http.Client, vaultAddr, vaultToken string) (*api.Client, error) {
 
 	client, err := api.NewClient(&api.Config{Address: vaultAddr, HttpClient: httpClient})
+
 	if err != nil {
 		return nil, err
 	}
@@ -187,7 +194,32 @@ func cmdVaultListCerts(ctx *cli.Context) (err error) {
 		return err
 	}
 
-	client, err := NewVaultClient(vaultAddr, vaultToken)
+	debug := ctx.Bool("debug")
+
+	var httpClient = &http.Client{
+		Timeout: 10 * time.Second,
+	}
+
+	if debug {
+		rh := &redactrus.Hook{
+			AcceptedLevels: logrus.AllLevels,
+			RedactionList:  []string{"^(X-Vault-Token: ).+$"},
+		}
+
+		log := logrus.New()
+
+		textFormatter := new(prefixed.TextFormatter)
+		textFormatter.FullTimestamp = true
+		textFormatter.TimestampFormat = time.RFC822
+
+		log.SetFormatter(textFormatter)
+
+		log.AddHook(rh)
+
+		httpClient = clientInspect.NewClientWriter(nil, nil, log.Writer())
+	}
+
+	client, err := NewVaultClient(httpClient, vaultAddr, vaultToken)
 	if err != nil {
 		return err
 	}
@@ -276,7 +308,32 @@ func cmdVaultCert(ctx *cli.Context) (err error) {
 		return err
 	}
 
-	client, err := NewVaultClient(vaultAddr, vaultToken)
+	debug := ctx.Bool("debug")
+
+	var httpClient = &http.Client{
+		Timeout: 10 * time.Second,
+	}
+
+	if debug {
+		rh := &redactrus.Hook{
+			AcceptedLevels: logrus.AllLevels,
+			RedactionList:  []string{"^(X-Vault-Token: ).+$"},
+		}
+
+		log := logrus.New()
+
+		textFormatter := new(prefixed.TextFormatter)
+		textFormatter.FullTimestamp = true
+		textFormatter.TimestampFormat = time.RFC822
+
+		log.SetFormatter(textFormatter)
+
+		log.AddHook(rh)
+
+		httpClient = clientInspect.NewClientWriter(nil, nil, log.Writer())
+	}
+
+	client, err := NewVaultClient(httpClient, vaultAddr, vaultToken)
 	if err != nil {
 		return err
 	}
